7a41bb103e3d5e9275aed5dc8a8ddd57ac70f067
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-06-01 15:08:53 +0200

    add install script for tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,4 +2,10 @@
 
 set -e # Stops the script if any part in it fails
 
-ln -sf "${DOTFILES_LOCATION}/tmux/.tmux.conf" "${HOME}/.tmux.conf"
+ln -sf "${DOTFILES_LOCATION}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+mkdir "${HOME}/.tmux/plugins/tpm"
+
+echo "Installing TPM"
+git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+
+echo "Done, open tmux and press CTRL I to install plugins"
