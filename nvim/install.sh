a71ad6972d965ebb267eb0e30d2362442f7432ad
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-06-05 11:24:17 +0200

    fix installation scripts paths

--- nvim/install.sh
+++ nvim/install.sh
@@ -7,6 +7,6 @@ git clone https://github.com/NvChad/NvChad "{$HOME}/.config/nvim" --depth 1
 
 echo "Linking custom config into NvChad"
 
-ln -sfr "${DOTFILES_LOCATION}/nvim/custom" "{$HOME}/.config/nvim"
+ln -sfr "${DOTFILES_LOCATION}/nvim/custom" "$HOME/.config/nvim"
 
 echo "Done, run nvim. Do not load any sample config, run :Lazy Sync and :MasonInstallAll to install plugins, lsps, formatters and so on"
--- starship/install.sh
+++ starship/install.sh
@@ -2,4 +2,4 @@
 
 set -e # Stops the script if any part in it fails
 
-ln -sf "${DOTFILES_LOCATION}/starship/starship.toml" "${HOME}/.config/starship.toml"
+ln -sf "$DOTFILES_LOCATION/starship/starship.toml" "$HOME/.config/starship.toml"
