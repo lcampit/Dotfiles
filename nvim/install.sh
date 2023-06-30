5f449f25aec9e4fc5bab929516c48038c8cb7e9c
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-06-30 17:07:37 +0200

    fix nvim install script

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,10 +3,10 @@
 set -e # Stops the script if any part of it fails
 
 echo "Installing NvChad"
-git clone https://github.com/NvChad/NvChad "{$HOME}/.config/nvim" --depth 1
+git clone https://github.com/NvChad/NvChad "$HOME/.config/nvim" --depth 1
 
 echo "Linking custom config into NvChad"
 
-ln -sfr "${DOTFILES_LOCATION}/nvim/custom" "$HOME/.config/nvim"
+ln -sfr "$DOTFILES_LOCATION/nvim/custom" "$HOME/.config/nvim/lua"
 
 echo "Done, run nvim. Do not load any sample config, run :Lazy Sync and :MasonInstallAll to install plugins, lsps, formatters and so on"
