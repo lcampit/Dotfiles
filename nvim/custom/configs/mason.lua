90fa547a62e27087f9cbc91a6e80bb692f0621f1
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-07-10 16:09:33 +0200

    move mason opts in own config file

---
+++ nvim/custom/configs/mason.lua
@@ -0,0 +1,18 @@
+local opts = {
+	ensure_installed = {
+		"gopls",
+		"rust-analyzer",
+		"pyright",
+		"mypy",
+		"ruff",
+		"black",
+		"debugpy",
+		"typescript-language-server",
+		"stylua",
+		"tailwindcss-language-server",
+		"deno",
+		"html-lsp",
+	},
+}
+
+return opts
--- nvim/custom/plugins.lua
+++ nvim/custom/plugins.lua
@@ -38,22 +38,7 @@ local plugins = {
 	},
 	{
 		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"gopls",
-				"rust-analyzer",
-				"pyright",
-				"mypy",
-				"ruff",
-				"black",
-				"debugpy",
-				"typescript-language-server",
-				"stylua",
-				"tailwindcss-language-server",
-				"deno",
-				"html-lsp",
-			},
-		},
+		opts = require("custom.configs.mason"),
 	},
 	{
 		"mfussenegger/nvim-dap",
