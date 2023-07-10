0197b6b12002bea2d6462216c237c7265f4ed710
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-07-10 17:49:56 +0200

    add mason-lspconfig and refactor

--- nvim/custom/configs/mason.lua
+++ nvim/custom/configs/mason.lua
@@ -1,18 +1,20 @@
-local opts = {
-	ensure_installed = {
-		"gopls",
-		"rust-analyzer",
-		"pyright",
-		"mypy",
-		"ruff",
-		"black",
-		"debugpy",
-		"typescript-language-server",
-		"stylua",
-		"tailwindcss-language-server",
-		"deno",
-		"html-lsp",
+require("mason").setup({
+	ui = {
+		check_outdated_packages_on_open = false,
+		border = "rounded",
+		icons = {
+			package_installed = " ",
+			package_pending = "󰦖 ",
+			package_uninstalled = "",
+		},
+		keymaps = {
+			toggle_server_expand = "<CR>",
+			install_server = "i",
+			update_server = "u",
+			check_server_version = "c",
+			update_all_servers = "U",
+			check_outdated_servers = "C",
+			uninstall_server = "X",
+		},
 	},
-}
-
-return opts
+})
