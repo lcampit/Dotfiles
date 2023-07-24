51d4d67a37b65f1f41da05ec9e37d90fe99afa85
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-07-24 17:18:23 +0200

    add wezterm keys and docs

---
+++ wezterm/README.md
@@ -0,0 +1,8 @@
+## Wezterm
+
+[Wezterm](https://wezfurlong.org/wezterm/index.html) is an amazing terminal
+emulator written in Rust and completely configurable in lua. It also comes with
+multiplexer features out of the box, supporting panes, windows and tabs.
+
+I've tinkered a bit with its configuration taking ideas from
+[this](https://github.com/Aylur/dotfiles/tree/main) fellow developer dotfiles.
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,15 +2,15 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.font = wezterm.font("JetBrainsMono NerdFont")
 config.color_scheme = "Catppuccin Mocha"
 
 config.window_frame = {
-  active_titlebar_bg = "#333333",
-  inactive_titlebar_bg = "#333333",
+	active_titlebar_bg = "#333333",
+	inactive_titlebar_bg = "#333333",
 }
 config.use_fancy_tab_bar = true
 config.default_cursor_style = "SteadyBar"
@@ -21,18 +21,21 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.enable_scroll_bar = false
 config.window_padding = {
-  top = "0.5cell",
-  right = "1cell",
-  bottom = "0.5cell",
-  left = "1cell",
+	top = "0.5cell",
+	right = "1cell",
+	bottom = "0.5cell",
+	left = "1cell",
 }
 
 config.inactive_pane_hsb = {
-  saturation = 0.9,
-  brightness = 0.8,
+	saturation = 0.9,
+	brightness = 0.8,
 }
 
 config.window_background_opacity = 1.0
 config.text_background_opacity = 1.0
+
+-- Load keys from outer file
+config.keys = require("keys.lua")
 
 return config
