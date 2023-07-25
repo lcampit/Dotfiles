0fdf95fa74caaa1140b76b857439b98a717be1fb
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-07-25 12:37:34 +0200

    wip for node version management and lsp, plus restore normal diagnostics

--- nvim/custom/init.lua
+++ nvim/custom/init.lua
@@ -2,6 +2,11 @@ vim.opt.colorcolumn = "80"
 vim.wo.relativenumber = true
 vim.opt.scrolloff = 30
 
+-- Locks node version used to latest
+-- vim.g.node_host_prog = "/usr/local/bin/neovim-node-host"
+-- vim.g.loaded_node_provider = 1
+-- vim.env.PATH = "$HOME/.local/share/rtx/installs/node/20.4.0/bin/node" .. vim.env.PATH
+
 -- Allows for lsp diagnostics to hover above error / warning / info / etc
-vim.o.updatetime = 250
-vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
+-- vim.o.updatetime = 250
+-- vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
