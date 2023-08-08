0aacb56e1761f7fe10a6d96334503818604b3dab
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-08 22:14:39 +0200

    update zellij installation script and config

--- src/zellij_command.sh
+++ src/zellij_command.sh
@@ -1,4 +1,19 @@
-echo "# this file is located in 'src/zellij_command.sh'"
-echo "# code for 'lcdot zellij' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+backup=${args[--backup]}
+install=${args[--install]}
+DOTFILES_LOCATION=$PWD
+if [ "$install" ]; then
+    echo "Installing zellij"
+    cargo install zellij --locked
+fi
+
+mkdir -p "$HOME"/.config/zellij
+echo "Installing zellij configuration from $DOTFILES_LOCATION"
+if [ "$backup" ]; then
+    echo "Moving previous configurations in $LCDOT_BACKUP/zellij"
+    mkdir -p "$LCDOT_BACKUP"/zellij
+
+    mv "$HOME"/.config/zellij/config.kdl "$LCDOT_BACKUP"/zellij/config.kdl
+fi
+
+echo "Setting up zellij configuration"
+ln -sf "$DOTFILES_LOCATION"/zellij/config.kdl "$HOME"/.config/zellij/config.kdl
