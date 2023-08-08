4e9cd58aa36d6568e1c35394c8de2885b1f3bca2
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-08 21:44:31 +0200

    add porsmo installation script

--- src/porsmo_command.sh
+++ src/porsmo_command.sh
@@ -1,4 +1,8 @@
-echo "# this file is located in 'src/porsmo_command.sh'"
-echo "# code for 'lcdot porsmo' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+install=${args[--install]}
+DOTFILES_LOCATION=$PWD
+if [ "$install" ]; then
+    echo "Installing porsmo"
+    cargo install porsmo
+fi
+
+echo "There is no configuration to install for porsmo, congrats!"
