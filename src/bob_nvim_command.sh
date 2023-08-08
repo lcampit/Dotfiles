59158ee3d91fe07b7193b5bae8be0f08c28e7997
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-08 19:54:55 +0200

    add bob-nvim config and script

--- src/bob_nvim_command.sh
+++ src/bob_nvim_command.sh
@@ -1,4 +1,24 @@
-echo "# this file is located in 'src/bob_nvim_command.sh'"
-echo "# code for 'lcdot bob-nvim' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+backup=${args[--backup]}
+install=${args[--install]}
+DOTFILES_LOCATION=$PWD
+if [ "$install" ]; then
+    echo "Installing bob-nvim "
+    cargo install bob-nvim
+fi
+echo "Installing bob-nvim configuration from $DOTFILES_LOCATION"
+
+bob_config_file=$HOME/.config/bob/config.json
+if [ "$BOB_CONFIG" ]; then
+    bob_config_file=$BOB_CONFIG
+fi
+
+mkdir -p "$(dirname "$bob_config_file")"
+
+if [ "$backup" ]; then
+    echo "Moving previous configurations in $LCDOT_BACKUP/bob-nvim"
+    mkdir -p "$LCDOT_BACKUP"/bob-nvim
+    mv "$bob_config_file" "$LCDOT_BACKUP"/bob-nvim/config.json
+fi
+
+echo "Setting up bob-nvim configuration"
+ln -sf "$DOTFILES_LOCATION"/bob-nvim/config.json "$bob_config_file"
