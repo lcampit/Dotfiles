21b8b3f534ae275080b0764027c878b34428bc52
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-08 20:23:30 +0200

    refactor git config install script with delta pager

--- src/delta_command.sh
+++ src/delta_command.sh
@@ -1,4 +1,25 @@
-echo "# this file is located in 'src/delta_command.sh'"
-echo "# code for 'lcdot delta' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+backup=${args[--backup]}
+install=${args[--install]}
+DOTFILES_LOCATION=$PWD
+if [ "$install" ]; then
+    echo "Installing git-delta"
+
+    if [ "$ARCHBTW"=true ]; then
+        echo "Arch based distro detected, installing using pacman"
+        sudo pacman -S git-delta
+
+    else
+        echo "Installing bat with cargo"
+        cargo install git-delta
+    fi
+fi
+echo "Installing git configuration from $DOTFILES_LOCATION"
+
+if [ "$backup" ]; then
+    echo "Moving present gitconfig file to $LCDOT_BACKUP/gitconfig"
+    mkdir -p "$LCDOT_BACKUP"/gitconfig
+    mv "$HOME"/.gitconfig "$LCDOT_BACKUP"/gitconfig/gitconfig.old
+fi
+echo "Linking gitconfig file in home directory"
+ln -sf "$DOTFILES_LOCATION/gitconfig/gitconfig" "$HOME/.gitconfig"
+echo "Done"
