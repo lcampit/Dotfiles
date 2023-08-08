7d6936c57c7651bd3f1aea21b59abc1747846c44
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-08 21:56:57 +0200

    add ncspot installation and configuration

---
+++ src/ncspot_command.sh
@@ -0,0 +1,19 @@
+install=${args[--install]}
+backup=${args[--backup]}
+DOTFILES_LOCATION=$PWD
+
+if [ "$install" ]; then
+    echo "Installing ncspot"
+    cargo install ncspot --locked
+fi
+
+if [ "$backup" ]; then
+    echo "Moving previous configurations in $LCDOT_BACKUP/ncspot"
+    mkdir -p "$LCDOT_BACKUP"/rust-tools/ncspot
+
+    mv "$HOME"/.config/ncspot "$LCDOT_BACKUP"/rust-tools/ncspot
+fi
+
+echo "Setting up ncspot configuration"
+ln -sfn "$DOTFILES_LOCATION/ncspot/" "$HOME/.config/ncspot"
+echo "Done"
