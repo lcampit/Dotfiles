410ee9fa33dd9ddfadc2221b3ede5f880938069e
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-08 21:08:24 +0200

    update gitui installation script

--- src/gitui_command.sh
+++ src/gitui_command.sh
@@ -1,4 +1,24 @@
-echo "# this file is located in 'src/gitui_command.sh'"
-echo "# code for 'lcdot gitui' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+backup=${args[--backup]}
+install=${args[--install]}
+DOTFILES_LOCATION=$PWD
+if [ "$install" ]; then
+    echo "Installing gitui"
+
+    if [ "$ARCHBTW"=true ]; then
+        echo "Arch based distro detected, installing using pacman"
+        sudo pacman -S gitui
+
+    else
+        echo "Installing gitui with cargo"
+        sudo cargo install gitui
+    fi
+fi
+
+if [ "$backup" ]; then
+    echo "Moving present gitconfig file to $LCDOT_BACKUP/gitui"
+    mkdir -p "$LCDOT_BACKUP"/gitui
+    mv "$HOME"/.config/gitui/* "$LCDOT_BACKUP"/gitui/
+fi
+echo "Setting up gitui configuration"
+ln -sfn "$DOTFILES_LOCATION/gitui/" "$HOME/.config/gitui"
+echo "Done"
