3a834cf12ea2cfafec1a5929815ef1c8a6d593e1
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-08-08 21:29:49 +0200

    refactor mprocs installation script

--- src/mprocs_command.sh
+++ src/mprocs_command.sh
@@ -1,4 +1,15 @@
-echo "# this file is located in 'src/mprocs_command.sh'"
-echo "# code for 'lcdot mprocs' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+install=${args[--install]}
+DOTFILES_LOCATION=$PWD
+if [ "$install" ]; then
+    echo "Installing mprocs"
+
+    if [ "$ARCHBTW"=true ]; then
+        echo "Arch based distro detected, installing using pacman"
+        sudo pacman -S mprocs
+
+    else
+        echo "Installing lazygit with cargo"
+        cargo install mprocs
+    fi
+fi
+echo "There is no configuration to install for mprocs, congrats!"
