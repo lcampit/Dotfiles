9de4b3a9ea521fae281969b0be7c011eb9c5e90e
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-11-15 17:08:08 +0100

    remove python host prog definition

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python3_host_prog = "/usr/bin/python3"
+-- vim.g.python3_host_prog = "/usr/bin/python3"
 vim.g.node_host_prog = "$HOME/.local/share/rtx/installs/node/latest/bin/neovim-node-host"
 vim.g.ruby_host_prog = "$HOME/.local/share/rtx/installs/ruby/latest/bin/neovim-ruby-host"
 
