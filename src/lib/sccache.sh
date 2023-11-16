dc256f331648142874958fe00e0e4efe60367fef
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-11-16 14:12:27 +0100

    update sccache config to use env vars

--- src/lib/sccache.sh
+++ src/lib/sccache.sh
@@ -1,21 +1,19 @@
 install_sccache() {
-    install=${args[--install]}
-    backup=${args[--backup]}
-    DOTFILES_LOCATION=$PWD
+	install=${args[--install]}
+	backup=${args[--backup]}
+	DOTFILES_LOCATION=$PWD
 
-    if [ "$install" ]; then
-        echo "Installing sccache"
-        cargo install sccache --locked
-    fi
+	if [ "$install" ]; then
+		echo "Installing sccache"
+		cargo install sccache --locked
+	fi
 
-    if [ "$backup" ]; then
-        echo "Moving previous configuration in $LCDOT_BACKUP/cargo"
-        mkdir -p "$LCDOT_BACKUP"/cargo
+	if [ "$backup" ]; then
+		echo "Moving previous configuration in $LCDOT_BACKUP/cargo"
+		mkdir -p "$LCDOT_BACKUP"/cargo
 
-        mv "$HOME"/.cargo/config.toml "$LCDOT_BACKUP"/cargo/
-    fi
+		mv "$HOME"/.cargo/config.toml "$LCDOT_BACKUP"/cargo/
+	fi
 
-    echo "Setting up cargo to use sccache"
-    ln -sf "$DOTFILES_LOCATION"/cargo/config.toml "$HOME"/.cargo/config.toml
-    echo "Done"
+	echo "Done"
 }
