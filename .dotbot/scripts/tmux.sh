d996757be89313102fcd611cfc3d4dd98d85e4e8
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-12-01 15:45:48 +0100

    add tpm installation to tmux script

--- .dotbot/scripts/tmux.sh
+++ .dotbot/scripts/tmux.sh
@@ -3,4 +3,7 @@ git clone https://github.com/tmux/tmux.git $HOME/tmux
 cd tmux
 sh autogen.sh
 ./configure && make
+
+mkdir -p ~/.config/tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 cd path
