a64d1e0ee3a8528739ac9b9aabaeaacea7d30941
Leonardo Campitelli <leonardo932.campitelli@gmail.com>
2023-12-02 21:49:31 +0100

    Update lazygit.sh

--- .dotbot/scripts/lazygit.sh
+++ .dotbot/scripts/lazygit.sh
@@ -4,8 +4,8 @@ if command -v pacman; then
 else
 
 	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": "v\K[^"]*')
-	curl -Lo $HOME/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-	tar xf $HOME/lazygit.tar.gz -C $HOME/lazygit/
-	sudo install $HOME/lazygit/lazygit /usr/local/bin
-	rm $HOME/lazygit.tar.gz $HOME/lazygit/lazygit
+	curl -Lo ./lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf ./lazygit.tar.gz 
+	sudo install lazygit /usr/local/bin
+	rm ./lazygit.tar.gz ./LICENSE ./README.md ./lazygit 
 fi
