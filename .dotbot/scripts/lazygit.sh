23a60ba069ddf53192ebfe310397c5ba32445707
Leonardo Campitelli <leonardo932.campitelli@gmail.com>
2023-12-05 12:38:36 +0100

    Update lazygit.sh

--- .dotbot/scripts/lazygit.sh
+++ .dotbot/scripts/lazygit.sh
@@ -1,11 +1,12 @@
 if command -v pacman; then
-	sudo pacman -S lazygit
+         pacman -S lazygit
 
 else
-	mkdir -p /tmp
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": "v\K[^"]*')
-	curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-	tar xf /tmp/lazygit.tar.gz 
-	sudo install /tmp/lazygit /usr/local/bin
-	rm /tmp/lazygit.tar.gz /tmp/LICENSE /tmp/README.md /tmp/lazygit 
+        mkdir -p /tmp
+        LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": "v\K[^"]*')
+        curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+        cd /tmp
+        tar xf lazygit.tar.gz
+        sudo install lazygit /usr/local/bin
+        rm lazygit.tar.gz LICENSE README.md lazygit
 fi
