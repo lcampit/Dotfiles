76e65f4ec70a1229d414bf6608bf28c57dd48d08
Leonardo Campitelli <leonardo932.campitelli@gmail.com>
2023-12-05 12:46:55 +0100

    Update tmux.sh

--- .dotbot/scripts/tmux.sh
+++ .dotbot/scripts/tmux.sh
@@ -1,9 +1,8 @@
-local path=$(pwd)
-git clone https://github.com/tmux/tmux.git $HOME/tmux
+mkdir -p /tmp
+git clone https://github.com/tmux/tmux.git /tmp/tmux
 cd tmux
 sh autogen.sh
 ./configure && make
 
 mkdir -p ~/.config/tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
-cd path
