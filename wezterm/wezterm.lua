ee52cb744736c8d10e29bd9b356797c381718bf0
Leo-Campo <leonardo932.campitelli@gmail.com>
2023-12-14 17:13:21 +0100

    add wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -24,27 +24,13 @@ config.underline_thickness = 3
 config.cursor_thickness = 4
 config.underline_position = -6
 
-if wezterm.target_triple:find("windows") then
-	config.default_prog = { "pwsh" }
-	config.window_decorations = "RESIZE|TITLE"
-	wezterm.on("gui-startup", function(cmd)
-		local screen = wezterm.gui.screens().active
-		local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-		local gui = window:gui_window()
-		local width = 0.7 * screen.width
-		local height = 0.7 * screen.height
-		gui:set_inner_size(width, height)
-		gui:set_position((screen.width - width) / 2, (screen.height - height) / 2)
-	end)
-else
-	config.term = "wezterm"
-	config.window_decorations = "RESIZE"
-end
+config.term = "wezterm"
+config.window_decorations = "RESIZE"
 
 -- Fonts
 config.font_size = 11
-config.font = wezterm.font({ family = "Fira Code" })
-config.bold_brightens_ansi_colors = true
+config.font = wezterm.font({ family = "JetBrains Mono" })
+config.bold_brightens_ansi_colors = false
 config.font_rules = {
 	{
 		intensity = "Bold",
@@ -66,7 +52,7 @@ config.font_rules = {
 -- Cursor
 config.default_cursor_style = "BlinkingBar"
 config.force_reverse_video_cursor = true
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.window_padding = { left = 5, right = 5, top = 0, bottom = 0 }
 -- window_background_opacity = 0.9,
 -- cell_width = 0.9,
 config.scrollback_lines = 10000
