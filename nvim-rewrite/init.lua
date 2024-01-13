528705148ec08c75cf11ba2079ad4be0a1b50ecf
Leo-Campo <leonardo932.campitelli@gmail.com>
2024-01-13 20:16:48 +0100

    add tokyonight storm colorscheme

---
+++ nvim-rewrite/init.lua
@@ -0,0 +1,19 @@
+-- automatically install lazy.nvim if it doesn't exist
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- set leader key before loading lazy
+vim.g.mapleader = " "
+
+-- let the magic happen
+require("lazy").setup("plugins")
