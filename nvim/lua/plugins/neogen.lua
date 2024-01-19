f6fe1c72ffd677c0f528c3b04bb4fd63fa1e6363
Leo-Campo <leonardo932.campitelli@gmail.com>
2024-01-19 12:32:26 +0100

    add neogen plugin

---
+++ nvim/lua/plugins/neogen.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"danymat/neogen",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = true,
+		keys = {
+			{
+				"<leader>cn",
+				"<cmd> lua require('neogen').generate()<cr>",
+				desc = "Generate code annotation",
+			},
+		},
+	},
+}
