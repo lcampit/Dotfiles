1e8618ee09a1eae208afb92b2112b3a96671ffe4
lcampit <leonardo932.campitelli@gmail.com>
2026-08-03 09:31:35 +0200

    fix: update build flags for debugging

--- editor/.config/nvim/lua/plugins/go.lua
+++ editor/.config/nvim/lua/plugins/go.lua
@@ -1,15 +1,43 @@
 -- Lsp configurations used to work with go files
 
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      gopls = {
-        settings = {
-          gopls = {
-            gofumpt = true,
-            buildFlags = { "-tags=integration" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              buildFlags = { "-tags=integration" },
+            },
           },
+        },
+      },
+    },
+  },
+  {
+    "leoluz/nvim-dap-go",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    dap_configurations = {
+      {
+        type = "go",
+        name = "Debug (Build Flags & Arguments)",
+        request = "launch",
+        program = "${file}",
+        args = require("dap-go").get_arguments,
+        buildFlags = require("dap-go").get_build_flags,
+      },
+    },
+    opts = {
+      delve = {
+        args = {
+          "--check-go-version=false",
+        },
+        build_flags = {
+          "-tags=integration",
         },
       },
     },
